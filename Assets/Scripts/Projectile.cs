9a60f2b79389a72d49be45de599060e5e206fdfa
Petur Darri <peturpokus99@gmail.com>
2017-01-07 20:36:48 +0000

    Added tower targeting

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -5,12 +5,29 @@ using UnityEngine;
 public class Projectile : MonoBehaviour {
 
 	public float baseSpeed;
-	public Transform tower;
 	private float radius = 5f;
 	private float force;
+	private Transform targetTower;
 	// Use this for initialization
 	void Start () {
-		tower = GameObject.Find ("BaseA").transform;
+		TowerManager enemyTowers;
+		var player = ScorchGameManager.Instance.currentPlayer;
+		if (player == Players.A) {
+			enemyTowers = ScorchGameManager.Instance.playerBTowers;
+		} else {
+			enemyTowers = ScorchGameManager.Instance.playerATowers;
+		}
+
+		var i = 0;
+		var j = 0;
+		foreach (var tower in enemyTowers.towers) {
+			if (Vector3.Distance(transform.position, tower.transform.position) < Vector3.Distance(transform.position, enemyTowers.towers[i].transform.position)) {
+				i = j;
+			}
+			j++;
+		}
+
+		targetTower = enemyTowers.towers [i].transform;
 	}
 	
 	// Update is called once per frame
@@ -19,7 +36,7 @@ public class Projectile : MonoBehaviour {
 		transform.Rotate (Vector3.right, 90);
 
 		//Guide
-		Vector3 magnetField = tower.position- transform.position;
+		Vector3 magnetField = targetTower.position- transform.position;
 		float index = (radius-magnetField.magnitude)/radius;
 		GetComponent<Rigidbody>().AddForce(force*magnetField*index);
 	}
