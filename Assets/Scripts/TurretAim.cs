a628c44ad407937f0357cbeb609a864e8f35fdf1
Owen Hindley <owenhindley@hotmail.com>
2017-01-07 12:52:28 +0000

    force text all the things

---
+++ Assets/Scripts/TurretAim.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretAim : MonoBehaviour {
+
+	public Transform followTarget;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.gameObject.transform.LookAt(followTarget);
+	}
+}
---
+++ Assets/Scripts/TurretFire.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurretFire : MonoBehaviour {
+
+	public float size;
+	public float aim;
+	public float rate;
+	public float multi;
+
+	public bool isFiring;
+
+	public GameObject projectile;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isFiring){
+
+
+
+		}
+	}
+}
