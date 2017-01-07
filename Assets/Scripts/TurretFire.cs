34d5fa3856abbb6c72b4f46589f510e89e43a03c
Owen Hindley <owenhindley@hotmail.com>
2017-01-07 12:53:37 +0000

    add ranges & calculation for fire rate

--- Assets/Scripts/TurretFire.cs
+++ Assets/Scripts/TurretFire.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class TurretFire : MonoBehaviour {
 
+	[Range(0.0f, 1.0f)]
 	public float size;
+	[Range(0.0f, 1.0f)]
 	public float aim;
+	[Range(0.0f, 1.0f)]
 	public float rate;
+	[Range(0.0f, 1.0f)]
 	public float multi;
 
 	public bool isFiring;
+
+	[Range(0.001f, 1.0f)]
+	public float minScale = 0.2f;
+	[Range(0.001f, 10.0f)]
+	public float maxScale = 1.0f;
+
+	[Range(0.001f, 1.0f)]
+	public float minFireInterval = 0.2f;
+	[Range(0.001f, 10.0f)]
+	public float maxFireInterval = 2.0f;
+
+	private float lastFireTime = Time.time;
 
 	public GameObject projectile;
 
@@ -22,6 +38,7 @@ public class TurretFire : MonoBehaviour {
 	void Update () {
 		if (isFiring){
 
+			float timeInterval = Mathf.Lerp(minFireInterval, maxFireInterval, Mathf.InverseLerp(0.0f, 1.0f, rate));
 
 
 		}
