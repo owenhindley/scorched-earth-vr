413626492261cd99663525d7e5a2ec31272e45b7
Petur Darri <peturpokus99@gmail.com>
2017-01-08 14:20:00 +0000

    Before the migration to main

---
+++ Assets/VRCameraWait.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRCameraWait : MonoBehaviour
+{
+
+    private LayerMask ogMasks;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    ogMasks = GetComponentInChildren<Camera>().cullingMask;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetWait(bool enable)
+    {
+        if (enable)
+        {
+            GetComponentInChildren<Camera>().cullingMask = LayerMask.NameToLayer("VR Wait");
+        }
+        else
+        {
+            GetComponent<Camera>().cullingMask = ogMasks;
+        }
+    }
+}
