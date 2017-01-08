b2dfccd3529104d81e3b0854e7e03c4132064482
Owen Hindley <owenhindley@hotmail.com>
2017-01-08 13:27:24 +0000

    add drunken gun again

---
+++ Assets/DrunkenAim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrunkenAim : MonoBehaviour {
+
+	public float speed = 1.0f;
+	public float amplitude = 0.01f;
+	public int octaves = 3;
+
+	private float wiggleIndex = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		wiggleIndex += speed * Time.deltaTime;
+		float angle = 0.0f;
+		for (int i=1; i < octaves; i++){
+			angle += (wiggleIndex * i)/i;
+
+		}
+		transform.RotateAround(Vector3.up, Mathf.Sin(angle) * amplitude);
+		transform.RotateAround(Vector3.left, Mathf.Cos(angle * 1.3f) * amplitude);
+		
+	}
+}
