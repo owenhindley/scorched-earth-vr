64663f5af84630cfb3f70c98f0215623de8b52b0
Owen Hindley <owenhindley@hotmail.com>
2017-01-08 14:42:23 +0000

    update cards

---
+++ Assets/Scripts/ScorchAudio.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorchAudio : MonoBehaviour {
+
+	public static ScorchAudio instance;
+
+	public List<AudioClip> fireSounds;
+	public List<AudioClip> impactSounds;
+	public AudioSource menuMove;
+	public AudioSource menuSelect;
+
+	public AudioSource fireSource;
+	public AudioSource impactSource;
+
+
+	// Use this for initialization
+	void Start () {
+		instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public static void PlayFire(){
+		if (instance){
+			AudioClip fire = (AudioClip)instance.fireSounds[Random.Range(0, instance.fireSounds.Count)];
+			instance.fireSource.PlayOneShot(fire);
+		}
+
+	}
+
+	public static void PlayImpact(){
+		if (instance){
+			AudioClip imp = (AudioClip)instance.impactSounds[Random.Range(0, instance.impactSounds.Count)];
+			instance.impactSource.PlayOneShot(imp);
+		}
+
+	}
+
+	public static void PlayMenuMove(){
+		if (instance){
+			instance.menuMove.Play();
+		}
+
+	}
+
+	public static void PlayMenuSelect(){
+		if (instance){
+			instance.menuSelect.Play();
+		}
+
+	}
+}
