7cc04dcb98e8cdf7f55fea4007408bb730aed724
Owen Hindley <owenhindley@hotmail.com>
2017-01-08 16:05:52 +0000

    create base score indicator

---
+++ Assets/Scripts/BaseScoreIndicator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class BaseScoreIndicator : MonoBehaviour {
+
+	public TextMeshPro counterA;
+	public Players whichPlayer;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+
+		if (ScorchGameManager.Instance){
+
+			TowerManager t;
+			if (whichPlayer == Players.A){
+				t = ScorchGameManager.Instance.playerATowers;
+			} else {
+				t = ScorchGameManager.Instance.playerBTowers;
+			}
+			 
+			counterA.SetText(t.towers.Count.ToString("00"));
+
+		}
+		
+	}
+}
